f48ccf81c5770167893506ee1eb1b2e01fca4f5b
Jorge Bucaran <jbucaran@me.com>
2015-07-23 19:03:07 +0900

    allow to return an object with lazify functions / props

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict';
+var slice = Array.prototype.slice
 module.exports = function (fn) {
-	return function (id) {
-		var mod;
+	return function (id, mod) {
 		return function () {
-			return mod !== undefined ? mod : mod = fn(id);
+			if (!arguments.length) return mod = lazy(mod, fn, id);
+			var obj = {}, args = slice.call(arguments, 0);
+			args.forEach(function (prop) {
+				Object.defineProperty(obj, prop, {
+					get: function () {
+						mod = lazy(mod, fn, id);
+						if (typeof mod[prop] === 'function') {
+							return function () {
+								return mod[prop].apply(mod, slice.call(arguments, 0));
+							}
+						} else {
+							return mod[prop];
+						}
+					}
+				});
+			});
+			return obj;
 		};
 	};
+	function lazy (mod, fn, id) {
+		return mod !== undefined ? mod : fn(id);
+	}
 };
